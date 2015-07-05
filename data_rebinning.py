95e3ea0284939f71eddc8b059a78988fed41cf1e
Leon Liang <jliang17@berkeley.edu>
2015-07-05 01:38:12 -0700

    added new comments

--- data_rebinning.py
+++ data_rebinning.py
@@ -1,5 +1,5 @@
 # @author Leon Liang
-
+#
 # Takes in [filename] that consists of all the data
 # and rebins the data into [factor] of data points
 # of the original data
@@ -8,15 +8,14 @@ import numpy as np
 import scipy as sp
 import sys
 
-def data_rebinning(data, factor): # rebin the data by a factor of
+def data_rebinning(data, factor):
     from scipy import ndimage
     from numpy import zeros
 
-    # factor = float(raw_input('What factor do you want to rebin the data by? '))
     [x,y] = data.shape
     new_x = int(round(x*factor))
 
-    rebinned_data = np.zeros([new_x,y]) # rebinning the data 1 column at a time
+    rebinned_data = np.zeros([new_x,y])
     for i in range(y):
         rebinned_data[:,i] = sp.ndimage.interpolation.zoom(data[:,i], factor)
 

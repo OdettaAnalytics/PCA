2b1e462ab4cd475b847e18f4b63dad4a0d31049f
Leon Liang <jliang17@berkeley.edu>
2015-11-12 00:50:19 -0800

    minor update

--- WiseRep/convert_HDF5.py
+++ WiseRep/convert_HDF5.py
@@ -5,12 +5,12 @@ import get_data
 
 def create_File(all_types):
 	for data_dir in all_types:
-		if not (os.path.isdir(data_dir + '/hdf5/')):
-				os.mkdir(data_dir + '/hdf5/')
+		if not (os.path.isdir(data_dir + '/hdf5_data/')):
+				os.mkdir(data_dir + '/hdf5_data/')
 
 	for data_dir in all_types:
 		data_type = data_dir.split('/')[1]
-		f = h5py.File(data_dir + '/hdf5/' + data_type + '.hdf5', 'w')
+		f = h5py.File(data_dir + '/hdf5_data/' + data_type + '.hdf5', 'w')
 		f.close()
 
 def convert_HDF5():
@@ -21,8 +21,8 @@ def convert_HDF5():
 		data_str = d.split('/')
 		data_type = data_str[1]
 		data_name = data_str[3]
-		data_file = h5py.File('supernova_data/' + data_type + '/hdf5/' + data_type + '.hdf5', 'a')
-		data_file_all = h5py.File('supernova_data/type_all/hdf5/type_all.hdf5', 'a')
+		data_file = h5py.File('supernova_data/' + data_type + '/hdf5_data/' + data_type + '.hdf5', 'a')
+		data_file_all = h5py.File('supernova_data/type_all/hdf5_data/type_all.hdf5', 'a')
 		spectrum = np.loadtxt(d)
 		data_file.create_dataset(data_name, data=spectrum)
 		data_file_all.create_dataset(data_type + ':' + data_name + '.hdf5', data=spectrum)

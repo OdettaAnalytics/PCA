dba9f30691b132813d10f23bdeeac0e371457887
Leon Liang <jliang17@berkeley.edu>
2015-12-02 16:55:05 -0800

    mostly working; able to get original model back; figuring out component accuracy

--- WiseRep/pca.py
+++ WiseRep/pca.py
@@ -6,8 +6,11 @@ import matplotlib.pyplot as plt
 import os, os.path, glob, sys, optparse, h5py
 import get_data
 
-def form_matrix():
-	dataset = get_data.all_hdf5()
+def form_matrix(category = None):
+	if category is not None:
+		dataset = get_data.hdf5(category)
+	else:
+		dataset = get_data.all_hdg5()
 	data_matrix = {}
 	for data in dataset:
 		data_mat = {}
@@ -33,41 +36,42 @@ def form_matrix():
 
 def normalize(data_matrix):
 	for data_type in data_matrix:
-		wavelength = data_matrix[data_type]['wavelength']
+		# wavelength = data_matrix[data_type]['wavelength']
 		flux = data_matrix[data_type]['flux']
-		for i in range(len(wavelength)):
-			wavelength /= np.linalg.norm(wavelength[i,:])
+		for i in range(len(flux)):
+			# wavelength /= np.linalg.norm(wavelength[i,:])
 			flux /= np.linalg.norm(flux[i,:])
 
 def compute_mean(data_matrix):
 	for data_type in data_matrix:
 		mu_matrix = {}
-		wavelength = data_matrix[data_type]['wavelength']
+		# wavelength = data_matrix[data_type]['wavelength']
 		flux = data_matrix[data_type]['flux']
-		wavelength_mu = np.zeros(wavelength.shape)
+		# wavelength_mu = np.zeros(wavelength.shape)
 		flux_mu = np.zeros(flux.shape)
-		for i in range(len(wavelength)):
-			wavelength_mu[i] = np.mean(wavelength[i,:])
+		for i in range(len(flux)):
+			# wavelength_mu[i] = np.mean(wavelength[i,:])
 			flux_mu[i] = np.mean(flux[i,:])
-		mu_matrix['wavelength'] = wavelength_mu
+		# mu_matrix['wavelength'] = wavelength_mu
 		mu_matrix['flux'] = flux_mu
 		data_matrix[data_type]['mu'] = mu_matrix
 
 
 def demean(data_matrix):
+	compute_mean(data_matrix)
 	for data_type in data_matrix:
-		wavelength = data_matrix[data_type]['wavelength']
+		# wavelength = data_matrix[data_type]['wavelength']
 		flux = data_matrix[data_type]['flux']
 		mu_matrix = data_matrix[data_type]['mu']
-		for i in range(len(wavelength)):
-			wavelength[i,:] -= mu_matrix['wavelength'][i]
+		for i in range(len(flux)):
+			# wavelength[i,:] -= mu_matrix['wavelength'][i]
 			flux[i,:] -= mu_matrix['flux'][i]
 
 def svd(data_matrix):
 	for data_type in data_matrix:
 		svd_mat = {}
 		# wavelength = data_matrix[data_type]['wavelength']
-		flux = data_matrix[data_type]['flux']
+		flux = data_matrix[data_type]['flux'].T
 		[U, s, V_T] = np.linalg.svd(flux)
 		[m, n] = flux.shape
 		S = np.zeros([m, n])
@@ -75,19 +79,21 @@ def svd(data_matrix):
 			for i in range(m):
 				S[i,i] = s[i] 											
 		else:
-			S[:n,:n] = diag(s)
+			S[:n,:n] = np.diag(s)
 		svd_mat['U'] = U
 		svd_mat['S'] = S
 		svd_mat['V'] = V_T.T
 		data_matrix[data_type]['svd'] = svd_mat
 
-def dot_product(data_matrix, data_type):
+def dot_product(data_matrix):
 	from numpy import dot
-	U = data_matrix[data_type]['svd']['U']
-	S = data_matrix[data_type]['svd']['S']
-	V = data_matrix[data_type]['svd']['V']
-	X_pca = U.dot(S).dot(V.T)
-	return X_pca
+	for data_type in data_matrix:
+		U = data_matrix[data_type]['svd']['U']
+		S = data_matrix[data_type]['svd']['S']
+		V = data_matrix[data_type]['svd']['V']
+		X_pca = U.dot(S).dot(V.T)
+		data_matrix[data_type]['x_pca'] = X_pca
+	# return X_pca
 
 def compute_pca(data_matrix):
 	from numpy import dot
@@ -99,7 +105,7 @@ def compute_pca(data_matrix):
 
 		flux = data_matrix[data_type]['flux']
 		pca_matrix = (V.dot(S.T)).T
-		coefficients = (U.T).dot(flux)
+		coefficients = (U.T).dot(flux.T)
 		data_matrix[data_type]['pca'] = pca_matrix
 		data_matrix[data_type]['coefficients']['normal'] = coefficients
 
@@ -110,8 +116,63 @@ def reduce_pca(data_matrix, n):
 		U_reduced = np.zeros(U.shape)
 		for i in range(n):
 			U_reduced[:,i] = U[:,i]
-		coefficients_reduced = (U_reduced.T).dot(flux)
+		coefficients_reduced = (U_reduced.T).dot(flux.T)
 		data_matrix[data_type]['svd']['U_reduced'] = U_reduced
 		data_matrix[data_type]['coefficients']['reduced'] = coefficients_reduced
+
+category = 'type_IIb'
+data_matrix = form_matrix(category)
+normalize(data_matrix)
+demean(data_matrix)
+svd(data_matrix)
+dot_product(data_matrix)
+compute_pca(data_matrix)
+reduce_pca(data_matrix, 4)
+
+
+
+# For testing solutions
+data_type = category
+coefficients = data_matrix[data_type]['coefficients']['normal']
+coefficients_reduced = data_matrix[data_type]['coefficients']['reduced']
+U = data_matrix[data_type]['svd']['U']
+S = data_matrix[data_type]['svd']['S']
+V = data_matrix[data_type]['svd']['V']
+wavelength = data_matrix[data_type]['wavelength']
+flux = data_matrix[data_type]['flux']
+U_reduced = data_matrix[data_type]['svd']['U_reduced']
+
+K = (U.dot(coefficients)).T
+K_reduce = (U_reduced.dot(coefficients_reduced)).T
+
+q = 6
+
+plt.plot(wavelength[q,:], flux[q,:])
+plt.plot(wavelength[q,:], K[q,:])
+plt.plot(wavelength[q,:], K_reduce[q,:])
+plt.show()
+
+
+# c1 = coefficients[0,:]
+# c2 = coefficients[1,:]
+# c3 = coefficients[2,:]
+# c4 = coefficients_reduced[3,:]
+# c5 = coefficients_reduced[4,:]
+
+# # reduced coefficients
+c1 = coefficients_reduced[0,:]
+c2 = coefficients_reduced[1,:]
+c3 = coefficients_reduced[2,:]
+# # c4 = coefficients_reduced[3,:]
+# c5 = coefficients_reduced[4,:]
+
+plt.scatter(c1, c2, color='blue')
+plt.scatter(c1, c3, color='red')
+# plt.scatter(c1, c4, color='green')
+plt.scatter(c2, c3, color='black')
+# plt.scatter(c2, c4, color='pink')
+# plt.scatter(c3, c4, color='yellow')
+
+plt.show()
 
 

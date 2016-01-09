ad180f5daf59481181294a0df4127c9e9826e6ed
Leon Liang <jliang17@berkeley.edu>
2016-01-08 22:32:13 -0800

    updated mkdir.py with more comments

--- WiseRep/util/mkdir.py
+++ WiseRep/util/mkdir.py
@@ -3,18 +3,58 @@ __author__ = 'Leon liang'
 '''
 This Python file will make necessary directories
 that will be used to store all data that other 
-Python files are looking for
+Python files will be locating
+
+init(): creates a initial directory "supernova_data" 
+that will later contain all of the different categories
+of supernovae and its data, plots, etc.
+
+data(): creates a directory that will house different
+categories of supernovae
+
+plots(): creates a directory that will be used to store
+all plots related to the specific supernova
 '''
 
 import os, glob
 import util.get_data as get_data
 
 def init(categories):
+	'''
+	categories: a list of supernova categories
+	'''
 	for category in categories:
 		if not os.path.isdir('supernova_data/' + category):
 			os.makekdirs('supernova_data/' + category + '/data/r')
 
+def data(category = None, kind = None):
+	'''
+	category: the supernova's category
+	kind: the data type that is needed (trimmed, demeaned, etc.)
+	'''
+	if category is not None:
+		if kind is not None:
+			if not os.path.isdir('supernova_data/' + category + '/data/' + kind):
+				os.makedirs('supernova_data/' + category + '/data/' + kind)
+		else:
+			if not os.path.isdir('supernova_data/' + category + '/data/'):
+				os.makedirs('supernova_data/' + category + '/data/')
+	else:
+		categoryDir = get_data.types()
+		for categoryDir in categoryDirs:
+			if kind is not None:
+				if not os.path.isdir(categoryDir + '/data/' + kind):
+					os.makedirs(categoryDir + '/data/' + kind)
+			else:
+				if not os.path.isdir(categoryDir + '/data'):
+					os.makedirs(categoryDir + '/data')
+
+
 def plots(category = None, kind = None):
+	'''
+	category: the supernova's category
+	kind: the data type that is needed (trimmed, demeaned, etc.)
+	'''
 	if category is not None:
 		if kind is not None:
 			if not os.path.isdir('supernova_data/' + category + '/plots/' + kind):
@@ -32,24 +72,6 @@ def plots(category = None, kind = None):
 				if not os.path.isdir(categoryDir + '/plots'):
 					os.makedirs(categoryDir + '/plots')
 
-
-def data(category = None, kind = None):
-	if category is not None:
-		if kind is not None:
-			if not os.path.isdir('supernova_data/' + category + '/data/' + kind):
-				os.makedirs('supernova_data/' + category + '/data/' + kind)
-		else:
-			if not os.path.isdir('supernova_data/' + category + '/data/'):
-				os.makedirs('supernova_data/' + category + '/data/')
-	else:
-		categoryDir = get_data.types()
-		for categoryDir in categoryDirs:
-			if kind is not None:
-				if not os.path.isdir(categoryDir + '/data/' + kind):
-					os.makedirs(categoryDir + '/data/' + kind)
-			else:
-				if not os.path.isdir(categoryDir + '/data'):
-					os.makedirs(categoryDir + '/data')
 
 # def remove(category=None, kind=None):
 

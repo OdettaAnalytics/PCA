005e2d9569a343d8343ca0845d8a10db2c278034
Leon Liang <jliang17@berkeley.edu>
2016-01-09 00:50:41 -0800

    updated to allow overwrite

--- WiseRep/util/convert_HDF5.py
+++ WiseRep/util/convert_HDF5.py
@@ -31,7 +31,11 @@ import h5py, util.mkdir as mkdir
 def create(data_category, data_name, data_type, spectrum):
 	mkdir.data(data_category)
 	data_file = h5py.File('supernova_data/' + data_category + '/data/' + data_type + '.hdf5')
-	data_file.create_dataset(data_name, data = spectrum)
+	if data_name in data_file.keys():
+		old_spectrum = data_file[data_name]
+		old_spectrum[...] = spectrum
+	else:
+		data_file.create_dataset(data_name, data = spectrum)
 	data_file.close()
 		
 if __name__ == '__main__':

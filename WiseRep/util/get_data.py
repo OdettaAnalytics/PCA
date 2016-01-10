f72ed0d58da002cdf8b1ba431bf94eae91d5c2c5
Leon Liang <jliang17@berkeley.edu>
2016-01-09 21:45:49 -0800

    changed most except hdf5 pathway

--- WiseRep/util/get_data.py
+++ WiseRep/util/get_data.py
@@ -8,51 +8,54 @@ that other functions can then utilize
 
 import glob, sys
 
-def trimmed(category = None):
-	if category is not None:
-		return glob.glob('supernova_data/' + category + '/data/trimmed_data/*')
-	else:
-		return glob.glob('supernova_data/type*/data/trimmed_data/*')
-
-def demeaned(category = None):
-	if category is not None:
-		return glob.glob('supernova_data/' + category + '/data/demeaned_data/*')
-	else:
-		return glob.glob('supernova_data/type*/data/demeaned_data/*')
-
-def deredshift(category = None):
-	if category is not None:
-		return glob.glob('supernova_data/' + category + '/data/deredshift_data/*')
-	else:
-		return glob.glob('supernova_data/type*/data/deredshift_data/*')
-
 def raw(category = None):
 	if category is not None:
 		return glob.glob('supernova_data/' + category + '/data/raw_data/*')
 	else:
 		return glob.glob('supernova_data/type*/data/raw_data/*')
 
+def trimmed(category = None):
+	if category is not None:
+		return glob.glob('supernova_data/' + category + '/data/*trimmed*')
+	else:
+		return glob.glob('supernova_data/type*/data/*trimmed*')
+
+def deredshift(category = None):
+	if category is not None:
+		return glob.glob('supernova_data/' + category + '/data/*deredshift*')
+	else:
+		return glob.glob('supernova_data/type*/data/*deredshift*')
+
+def demeaned(category = None):
+	if category is not None:
+		return glob.glob('supernova_data/' + category + '/data/*demeaned*')
+	else:
+		return glob.glob('supernova_data/type*/data/*demeaned')
+
 def linear(category = None):
 	if category is not None:
-		return glob.glob('supernova_data/' + category + '/data/linear_rebin_data/*')
+		return glob.glob('supernova_data/' + category + '/data/*linear_rebin*')
 	else:
-		return glob.glob('supernova_data/type*/data/linear_rebin_data/*')
+		return glob.glob('supernova_data/type*/data/*linear_rebin*')
 
 def log(category = None):
 	if category is not None:
-		return glob.glob('supernova_data/' + category + '/data/log_rebin_data/*')
+		return glob.glob('supernova_data/' + category + '/data/*log_rebin*')
 	else:
-		return glob.glob('supernova_data/type*/data/log_rebin_data/*')
+		return glob.glob('supernova_data/type*/data/*log_rebin*')
 
-def hdf5(category = None, type_all=False):
+def hdf5(category = None, type_all = False):
 	if category is not None:
 		return glob.glob('supernova_data/' + category + '/data/hdf5_data/*')
 	else:
 		if type_all:
 			return glob.glob('supernova_data/type*/data/hdf5_data/*')
 		else:
-			types = glob.glob('supernova_data/type*/data/hdf5_data/*')
-			types.remove('supernova_data/type_all/data/hdf5_data/type_all.hdf5')
+			# types = glob.glob('supernova_data/type*/data/hdf5_data/*')
+			# types.remove('supernova_data/type_all/data/hdf5_data/type_all.hdf5')
+			# return types
+			types = glob.glob('supernova_data/type*/hdf5_data/*')
+			types.remove('supernova_data/type_all/hdf5_data/type_all.hdf5')
 			return types
 
 def z_value():
@@ -68,7 +71,7 @@ def all_hdf5(category = None):
 	# for testing purposes:
 	# return glob.glob('test.hdf5')
 
-def types(category = None, data_type = None, type_all=False):
+def types(category = None, data_type = None, type_all = False):
 	if category is not None:
 		if data_type is not None:
 			if data_type == 'trimmed':

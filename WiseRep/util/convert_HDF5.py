6b92d83138fa0b133561465de69a09726c9a648d
Leon Liang <jliang17@berkeley.edu>
2016-01-10 23:02:44 -0800

    updated file to be able to overwrite with different dimension data

--- WiseRep/util/convert_HDF5.py
+++ WiseRep/util/convert_HDF5.py
@@ -4,10 +4,13 @@ import h5py, util.mkdir as mkdir
 
 def write(data_category, data_name, data_type, spectrum):
 	mkdir.data(data_category)
-	data_file = h5py.File('supernova_data/' + data_category + '/data/' + data_type + '.hdf5')
+	data_file = h5py.File('supernova_data/' + data_category + '/data/' + data_type + '.hdf5', 'a')
 	if data_name in data_file.keys():
-		old_spectrum = data_file[data_name]
-		old_spectrum[...] = spectrum
+		if data_file[data_name].shape != spectrum.shape:
+			data_file.__delitem__(data_name)
+			data_file[data_name] = spectrum
+		else:
+			data_file[data_name][...] = spectrum
 	else:
 		data_file.create_dataset(data_name, data = spectrum)
 	data_file.close()

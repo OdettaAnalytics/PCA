ca8e5bc3eb7515efc9ffedb820dde8430a3a895d
Leon Liang <jliang17@berkeley.edu>
2016-02-04 14:09:28 -0800

    updated input variables

--- WiseRep/util/convert_HDF5.py
+++ WiseRep/util/convert_HDF5.py
@@ -2,9 +2,9 @@ __author__ = 'Leon Liang'
 
 import h5py, util.mkdir as mkdir
 
-def write(data_category, data_name, data_type, spectrum):
+def write(data_category, data_name, data_filename, spectrum):
 	mkdir.data(data_category)
-	data_file = h5py.File('supernova_data/' + data_category + '/data/' + data_type + '.hdf5', 'a')
+	data_file = h5py.File('supernova_data/' + data_category + '/data/' + data_filename + '.hdf5', 'a')
 	if data_name in data_file.keys():
 		if data_file[data_name].shape != spectrum.shape:
 			data_file.__delitem__(data_name)
